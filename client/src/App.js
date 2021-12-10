4182f1e3c5dd8ad4e831fdbca81d2319bd192937
Sayandip Halder <sayandiph4@gmail.com>
2021-12-10 17:06:10 +0530

    setup server and basic client structure

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Typography, AppBar } from '@material-ui/core';
+
+const App = () => {
+    return (
+        <div>
+            Video chat app
+        </div>
+    )
+}
+
+export default App;
