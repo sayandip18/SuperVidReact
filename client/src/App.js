5fc56fe50d84b57612acf5c66e205117bc590f45
Sayandip Halder <sayandiph4@gmail.com>
2021-12-10 22:48:11 +0530

    socket logic

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,49 @@
 import React from 'react';
-import { Typography, AppBar } from '@material-ui/core';
+import Typography from '@mui/material/Typography';
+import AppBar from '@mui/material/AppBar';
+import VideoPlayer from './components/VideoPlayer';
+import Options from './components/Options';
+import Notifications from './components/Notifications';
+
+import makeStyles from '@mui/styles/makeStyles';
+
+const useStyles = makeStyles((theme) => ({
+    appBar: {
+      borderRadius: 15,
+      margin: '30px 100px',
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center',
+      width: '600px',
+      border: '2px solid black',
+  
+    //   [theme.breakpoints.down('xs')]: {
+    //     width: '90%',
+    //   },
+    },
+    image: {
+      marginLeft: '15px',
+    },
+    wrapper: {
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      width: '100%',
+    },
+}));
 
 const App = () => {
+    const classes = useStyles();
     return (
-        <div>
-            Video chat app
+        <div className={classes.wrapper}>
+            <AppBar className={classes.appBar} position="static" color="inherit">
+                <Typography variant="h2" align="center">Video Chat</Typography>
+            </AppBar>
+            <VideoPlayer />
+            <Options>
+                <Notifications />
+            </Options>
         </div>
     )
 }
